a695fbd696f1bc8733c92159d7755920268291ae
Denys Koval <denys_koval@epam.com>
2016-11-17 14:55:46 +0200

    add test class

---
+++ src/main/java/client/Hello.java
@@ -0,0 +1,10 @@
+package client;
+
+/**
+ * Created by Denys_Koval on 11/17/2016.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello webservice");
+    }
+}
